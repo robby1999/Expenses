bd5e0f10da981c4b4ba75566968fddd55ac2c651
Robby McCammon <robbydx123@hotmail.co.uk>
2022-05-17 11:11:49 +0100

    Added user input and Firebase functionality

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -1,7 +1,18 @@
-import { Text } from 'react-native';
+import { useContext } from "react";
+
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+import { ExpensesContext } from "../store/expenses-context";
 
 function AllExpenses() {
-    return <Text>AllExpenses Screen</Text>;
+  const expensesCtx = useContext(ExpensesContext);
+
+  return (
+    <ExpensesOutput
+      expenses={expensesCtx.expenses}
+      expensesPeriod="Total"
+      fallbackText="No registered expenses found!"
+    />
+  );
 }
 
 export default AllExpenses;

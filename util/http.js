18b5ac17d4302fd2639ebb85f2b762b2b3c0c199
robby1999 <robbydx123@hotmail.co.uk>
2022-05-25 12:08:17 +0100

    Update http.js

--- util/http.js
+++ util/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BACKEND_URL =
-  "https://react-native-course-c4db7-default-rtdb.firebaseio.com";
+  "https://react-native-expenses-ec5ee-default-rtdb.firebaseio.com";
 
 export async function storeExpense(expenseData) {
   const response = await axios.post(
